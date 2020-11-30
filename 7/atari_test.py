4642b871bc402756d55b6ea57b50cdb8c1b10074
test <w.pooimun@gmail.com>
2020-11-30 16:15:20 +0800

    paper set up

--- 7/atari_test.py
+++ 7/atari_test.py
@@ -13,7 +13,14 @@ def test():
             return
     env.close()
 
+def test2():
+    n=4
+    epsilon=0.2
+    p=np.random.default_rng().dirichlet(np.ones(n))*(1-epsilon)
+    p+=epsilon/n
+    print(p)
+
 if __name__ == "__main__":
-    test()
+    test2()
 
 
